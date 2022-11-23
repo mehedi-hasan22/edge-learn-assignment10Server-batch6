97e5f1381cccc94bce581440b6b28627bf2bee88
mehedi-hasan22 <mehedi30554@gmail.com>
2022-11-23 21:25:08 +0600

    send data to frontend

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+const categories = require('./data/categories.json');
+const courses = require('./data/Courses.json');
+
 app.get('/', (req, res) => {
     res.send('News API Running');
 });
+
+app.get('/categories', (req, res) => {
+    res.send(categories)
+});
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
+
+
 
 app.listen(port, () => {
     console.log('server running', port)
